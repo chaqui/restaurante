cc2f31627b2253f23b5ed15fa0a83fe8f7608778
Josue Chaqui <josue.chaqui@gmail.com>
2018-05-13 21:30:38 -0600

    creando proyecto

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+app.get('/',function(request,response)
+{
+  response.send('hola mundo')
+
+})
+
+app.listen(3000, function(){
+  console.log('server ready')
+})
