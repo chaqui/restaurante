da9be0a2d6e4ad347c679cb5d9c2fd9bce27f29c
Josue Chaqui <josue.chaqui@gmail.com>
2018-05-14 01:03:06 -0600

    agregando namespace

--- public/cocina/main.js
+++ public/cocina/main.js
@@ -1,12 +1,19 @@
-var socket = io.connect('http://localhost:3000', { 'forceNew': true });
+var socket = io.connect('http://localhost:3000/cocina', { 'forceNew': true });
 
 function render(data) {
 	var html = data.map(function(elem, index){
 			console.log(elem)
+			if (elem.llevar == undefined) {
+				return(`<div class="card"  style="width: 18rem;">
+							<div class="card-body">
+	        		 <strong>Normales:</strong>  <em>${elem.normales}</em>
+	              <strong>Infantiles:</strong>  <em>${elem.infantiles}</em>
+	        </div>
+					</div>
+		 </div>`)
+			}
     	return(`<div class="card"  style="width: 18rem;">
 						<div class="card-body">
-        		 <strong>Normales:</strong>  <em>${elem.normales}</em>
-              <strong>Infantiles:</strong>  <em>${elem.infantiles}</em>
               <strong>Llevar:</strong>  <em>${elem.llevar}</em>
         </div>
 				</div>
